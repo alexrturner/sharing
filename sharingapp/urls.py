b0c3894c11753d762e0029fe77e9d4b7900fd6db
alexrturner <50839467+alexrturner@users.noreply.github.com>
2023-11-23 13:28:40 +0800

    Configure media files

--- sharingapp/urls.py
+++ sharingapp/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 from sharing import views
-
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -33,3 +34,6 @@ urlpatterns = [
     # path('hardware/', views.Hardware_View.as_view()),
     # path('send_email/', views.send_email)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
